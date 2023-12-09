017b409b4e52123ff4e22bf6187c0a41d825959e
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-09 22:05:02 +0000

    Navigation added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-    <Container>
-      <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
+      <Navbar expand="lg" data-bs-theme="dark" className="bg-body-tertiary mb-4">
+        <Container>
+          <Navbar.Brand href="#">SmartHome Dashboard</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col className='column'>Current Time</Col>
+          <Col className='column'>Google Home Controls</Col>
+          <Col className='column'>Wifi Data</Col>
+        </Row>
+        <Row>
+          <Col className='column'>Live weather</Col>
+          <Col className='column'>Music</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
