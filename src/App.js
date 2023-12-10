a86a8f507dce99244ca954cad1972c0b7d5060d0
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-10 15:35:42 +0000

    Menu buttons added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Button from 'react-bootstrap/Button';
+import './App.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import CurrentTime from './CurrentTime';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <Navbar expand="lg" data-bs-theme="dark" className="bg-body-tertiary mb-4">
+      <Navbar expand="lg" data-bs-theme="dark" className="bg-body-tertiary mb-1">
         <Container>
-          <Navbar.Brand href="#">SmartHome Dashboard</Navbar.Brand>
+          {/* <Navbar.Brand href="#">SmartHome Dashboard</Navbar.Brand> */}
+          <Button variant="secondary" className='buttons m-2'>Refresh</Button>
+          <Button variant="secondary" className='buttons me-4'>Settings</Button>
         </Container>
       </Navbar>
       <Container>
