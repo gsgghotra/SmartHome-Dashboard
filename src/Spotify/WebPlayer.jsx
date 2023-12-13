ac3db8a3469ab9c947081ed46790e3d8e9fa9e93
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-13 22:33:37 +0000

    Added playback device button

--- src/Spotify/WebPlayer.jsx
+++ src/Spotify/WebPlayer.jsx
@@ -4,6 +4,7 @@ function WebPlayback(props) {
     const [player, setPlayer] = useState(undefined);
     const [is_paused, setPaused] = useState(false);
     const [is_active, setActive] = useState(false);
+    const [device, setDevice] = useState(0);
     const [current_track, setTrack] = useState({
         name: "Temporary Song",
         album: {
@@ -30,6 +31,7 @@ function WebPlayback(props) {
 
         player.addListener('ready', ({ device_id }) => {
             console.log('Ready with Device ID', device_id);
+            setDevice(device_id)
             setActive(true);
         });
 
@@ -42,6 +44,83 @@ function WebPlayback(props) {
         };
     }, [props.accessToken]);
 
+    const handlePlay = async () => {
+        try {
+            const trackUri = 'spotify:track:72vuBPMhwFNlSYpTSf6fVD'; // Replace TRACK_ID with the actual Spotify track ID
+            const response = await fetch('https://api.spotify.com/v1/me/player/play', {
+                method: 'PUT',
+                headers: {
+                'Authorization': 'Bearer ' + props.accessToken,
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                uris: [trackUri],
+                }),
+            });
+        
+            const data = await response.json();
+        
+            if (data.error) {
+                console.error('Error playing track:', data.error);
+            } else {
+                setPaused(false);
+            }
+            } catch (error) {
+            console.error('Error playing track:', error);
+            }
+        };
+
+    const handlePause = async () => {
+    try {
+        await fetch('https://api.spotify.com/v1/me/player/pause', {
+        method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + props.accessToken,
+        },
+        });
+        setPaused(true);
+    } catch (error) {
+        console.error('Error pausing track:', error);
+    }
+    };
+    
+    const handleSkip = async () => {
+    try {
+        await fetch('https://api.spotify.com/v1/me/player/next', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + props.accessToken,
+        },
+        });
+    } catch (error) {
+        console.error('Error skipping track:', error);
+    }
+    };
+
+    const handleTransferPlayback = async () => {
+        try {
+            const response = await fetch('https://api.spotify.com/v1/me/player', {
+                method: 'PUT',
+                headers: {
+                'Authorization': 'Bearer ' + props.accessToken,
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                device_ids: [device], // Use the ID of the current device
+                play: !is_paused, // Continue playing if not paused
+                }),
+            });
+        
+            const data = await response.json();
+        
+            if (data.error) {
+                console.error('Error transferring playback:', data.error);
+            }
+            } catch (error) {
+            console.error('Error transferring playback:', error);
+            }
+        };
+
     return (
         <>
         <div className="container">
@@ -53,6 +132,13 @@ function WebPlayback(props) {
 
             <p>Status: {is_active ? 'Active' : 'Inactive'}</p>
             <p>Paused: {is_paused ? 'Yes' : 'No'}</p>
+
+            <div>
+                <button onClick={handlePlay}>Play</button>
+                <button onClick={handlePause}>Pause</button>
+                <button onClick={handleSkip}>Skip</button>
+                <button onClick={handleTransferPlayback}>Transfer Playback</button>
+            </div>
             </div>
         </div>
         </>
